60e4045672d96e81b596f6619f7c86d7580e2cee
Your Name~ <you@example.com>
2024-02-18 19:26:26 +0600

    total price add

--- scripts/index.js
+++ scripts/index.js
@@ -10,5 +10,72 @@ for(const seat of allSeat){
         countSub = countSub - 1;
         document.getElementById('seat-count').innerText = countAdd;
         document.getElementById('seat-left').innerText = countSub;
+        seat.classList.add('bg-[#1DD100]', 'text-white');
+
+        const seatNo = e.target.innerText;     
+        const selectedContainer = document.getElementById('selected-container');
+                
+        // ---------------------------------- Seat info update on click
+        const economy = "Economy";
+        const price = 550;
+
+        const div = document.createElement("div");
+        const p = document.createElement("p");
+        p.innerText = seatNo;
+        const p2 = document.createElement("p");
+        p2.innerText = economy;
+        const p3 = document.createElement("p");
+        p3.innerText = price;
+
+        div.appendChild(p);
+        div.appendChild(p2);
+        div.appendChild(p3);
+
+        selectedContainer.appendChild(div)
+        div.classList.add('flex', 'justify-between', 'my-2');
+
+        // ----------------------------------- Total Price update
+        const totalPrice = document.getElementById('total-price');
+        totalPriceCount = price * countAdd;
+
+        totalPrice.innerText = totalPriceCount;
+
+        
+        // ----------------------------------- Grand Total Price
+        const grandTotalPrice = document.getElementById('grand-total');
+        const coupon = document.getElementById('coupon-input');
+        console.log(coupon.value);
+
+        grandTotalPriceCount = totalPriceCount;
+        grandTotalPrice.innerText = grandTotalPriceCount;
+        
     })
 }
+
+const grandTotalPrice = document.getElementById('grand-total');
+
+const inputBtn = document.getElementById('input-btn');
+inputBtn.addEventListener('click', function(){
+    // input value
+    const couponInput = document.getElementById('coupon-input').value;
+    console.log(couponInput);
+        
+    if(totalPriceCount >= 2200){
+        if(couponInput === NEW15){
+            const grandTotalPrice15 = totalPriceCount * .85;
+            grandTotalPrice.innerText = grandTotalPrice15;
+        }
+        else if(couponInput === Couple_20){
+
+        }
+    } 
+    else{
+        const grandTotalPrice = document.getElementById('grand-total');
+        const coupon = document.getElementById('coupon-input');
+        console.log(coupon.value);
+
+        grandTotalPriceCount = totalPriceCount;
+        grandTotalPrice.innerText = grandTotalPriceCount;
+    }
+    
+})
